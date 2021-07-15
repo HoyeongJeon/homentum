28f93fa113ce1c839ba8e34b9eb1ee303e7f88ed
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-16 01:20:25 +0900

    Making a todo list..(1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Clock } from "./Clock";
+import { TodoListItem } from "./TodoListItem";
+import { Todo } from "./types";
 
-function App() {
-  return <div className="App">Hi!</div>;
-}
+const todos: Array<Todo> = [
+  { text: "study react and typescript", complete: true },
+  { text: "sleep", complete: false },
+];
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      {/* <div className="Clock">
+        <Clock />
+      </div> */}
+      {/* Greeting */}
+      <>
+        <TodoListItem todo={todos[0]} />
+        <TodoListItem todo={todos[1]} />
+      </>
+      {/* Weather */}
+      {/* Quotes */}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Clock.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const time = () => {
+  const date = new Date();
+  console.log(date);
+};
+
+export const Clock: React.FC = () => {
+  return <div></div>;
+};
