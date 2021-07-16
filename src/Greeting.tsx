9541ef055e29af4e0e44607c2ca65dc741f69932
HoyeongJeon <hoyeong0302@gmail.com>
2021-07-16 16:24:35 +0900

    새로고침해도 유저이름 저장

--- src/Greeting.tsx
+++ src/Greeting.tsx
@@ -1,32 +1,51 @@
 import React, { ChangeEvent } from "react";
+import { useEffect } from "react";
+import { useCallback } from "react";
 import { useRef } from "react";
 import { useState } from "react";
 
 export const Greeting: React.FC = () => {
-  const [userInput, setUserInput] = useState("");
-  const [user, setUser] = useState("Good day !");
+  const USER_KEY = "User";
+  const USER_LS = localStorage.getItem(USER_KEY);
+  const [userInput, setUserInput] = useState<string>("");
+
   const onUserChange = (e: any) => {
     e.preventDefault();
-    setUserInput(e.target.value);
+    const name: string = e.target.value;
+    setUserInput(name);
   };
 
-  const onUserSubmit = (e: any) => {
-    e.preventDefault();
-    setUser(`Good day! ${userInput}`);
-    setUserInput("");
+  const onUserSubmit = () => {
+    localStorage.setItem(USER_KEY, userInput);
+  };
+  const getUser = () => {
+    const userInit = localStorage.getItem(USER_KEY);
+    if (userInit === null) {
+      return (
+        <>
+          <h1>Welcome!</h1>
+          <form onSubmit={onUserSubmit}>
+            <input
+              type="text"
+              placeholder="What is your name?"
+              value={userInput}
+              onChange={onUserChange}
+            />
+          </form>
+        </>
+      );
+    }
+    return (
+      <>
+        <h1>Good Day!</h1>
+        <h2>{USER_LS}</h2>
+      </>
+    );
   };
 
   return (
     <>
-      <h1>{user}</h1>
-      <form onSubmit={onUserSubmit}>
-        <input
-          type="text"
-          placeholder="What is your name?"
-          value={userInput}
-          onChange={onUserChange}
-        />
-      </form>
+      <div>{getUser()}</div>
     </>
   );
 };
